00b99d2eb0f87336bd5603963f78a6824d335995
quxiaodong <quxiaodong1990@gmail.com>
2024-10-02 16:19:45 +0800

    feat: 添加通用工具

---
+++ src/hooks/upload/config.ts
@@ -0,0 +1,21 @@
+export type Slice = {
+  index: number
+  total: number
+  type: string
+  retries: number
+  status: 'none' | 'upload' | 'success' | 'error'
+}
+
+export type Item = {
+  file: File
+  hash: string
+  slices: Slice[]
+  progress: number
+  status: 'none' | 'upload' | 'success' | 'error'
+  previewUrl: string
+  url?: string
+}
+
+export const MAX_SIZE = 20 * 1024
+
+export const MAX_RETRIES = 3
---
+++ src/utils/array-to-tree.ts
@@ -0,0 +1,33 @@
+interface TreeNode {
+  id: string
+  sort?: number | null
+  parentId?: string | null
+  children?: TreeNode[]
+}
+
+export function buildTree<T extends TreeNode>(array: T[]): T[] {
+  const sortedArray = [...array].sort(
+    (a, b) => (a.sort || Infinity) - (b.sort || Infinity)
+  )
+
+  const map = new Map<string, T>()
+  const roots: T[] = []
+
+  sortedArray.forEach(item => map.set(item.id, { ...item, children: [] }))
+
+  sortedArray.forEach(item => {
+    const node = map.get(item.id)!
+    if (item.parentId) {
+      const parent = map.get(item.parentId)
+      if (parent) {
+        parent.children?.push(node)
+      } else {
+        roots.push(node)
+      }
+    } else {
+      roots.push(node)
+    }
+  })
+
+  return roots
+}
