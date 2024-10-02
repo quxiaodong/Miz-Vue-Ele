737f90dcc0f14537e5d01393b80dd98de458a762
quxiaodong <quxiaodong1990@gmail.com>
2024-10-02 19:03:31 +0800

    feat: 添加通用弹窗

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
 import { App } from 'vue'
 import CommonDark from './common-dark/Index.vue'
+import CommonDialog from './common-dialog/Index.vue'
 import CommonLocale from './common-locale/Index.vue'
 
 const components = (app: App) => {
@@ -11,6 +12,7 @@ const components = (app: App) => {
   app.use(ElementPlus)
   app.component('CommonDark', CommonDark)
   app.component('CommonLocale', CommonLocale)
+  app.component('CommonDialog', CommonDialog)
 }
 
 export default components
