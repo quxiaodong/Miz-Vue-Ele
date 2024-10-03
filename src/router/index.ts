90b9d730a045ce1cfa5fcb654e7a0d2e27ba0d86
quxiaodong <quxiaodong1990@gmail.com>
2024-10-03 19:13:06 +0800

    feat: 注册登录页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,6 +23,36 @@ const router = createRouter({
       ]
     },
     {
+      path: '/sign-up',
+      component: () => import('@/views/iam/Index.vue'),
+      children: [
+        {
+          path: '',
+          meta: {
+            whitelist: true,
+            name: '注册',
+            stem: 'common:menu.sign-up'
+          },
+          component: () => import('@/views/iam/sign-up/Index.vue')
+        }
+      ]
+    },
+    {
+      path: '/sign-in',
+      component: () => import('@/views/iam/Index.vue'),
+      children: [
+        {
+          path: '',
+          meta: {
+            whitelist: true,
+            name: '登录',
+            stem: 'common:menu.sign-in'
+          },
+          component: () => import('@/views/iam/sign-in/Index.vue')
+        }
+      ]
+    },
+    {
       path: '/:pathMatch(.*)*',
       component: () => import('@/views/404/Index.vue')
     }
