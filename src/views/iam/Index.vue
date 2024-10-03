7a3bbbad9eeb7661476c57d03495c7769d2d3544
quxiaodong <quxiaodong1990@gmail.com>
2024-10-03 19:13:06 +0800

    feat: 注册登录页面

---
+++ src/views/iam/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="page-bg flex h-full items-center justify-center bg-[url('@/images/background.svg')] bg-contain bg-center bg-no-repeat"
+  >
+    <el-space :size="20" class="fixed right-5 top-5">
+      <common-dark />
+      <common-locale />
+    </el-space>
+    <div class="w-80 sm:w-4/5">
+      <img src="@/images/element-plus-logo.svg" class="mx-auto mb-10 w-1/2" />
+      <router-view />
+    </div>
+  </div>
+</template>
