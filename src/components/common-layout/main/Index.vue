f4aa58f87a633618b39d3f9516453681ba5b488e
quxiaodong <quxiaodong1990@gmail.com>
2024-10-04 16:51:13 +0800

    feat: 添加通用布局

---
+++ src/components/common-layout/main/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-scrollbar view-class="h-full">
+    <router-view v-slot="{ Component }">
+      <div class="box-border h-full p-4">
+        <transition
+          appear
+          mode="out-in"
+          :name="animate"
+          enter-active-class="transition-all duration-200"
+          leave-active-class="transition-all duration-200"
+        >
+          <keep-alive :include="caches">
+            <component :is="Component" />
+          </keep-alive>
+        </transition>
+        <!-- when main content very long, the bottom padding will be hidden -->
+        <div class="pb-4" />
+      </div>
+    </router-view>
+  </el-scrollbar>
+</template>
+
+<script setup lang="ts">
+import { animate } from '../stores/animate'
+import { caches } from '../stores/views'
+</script>
---
+++ src/components/common-layout/setting/main/History.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-form-item :label="translate('common:layout.setting.history')">
+    <div class="w-full text-right">
+      <el-switch :model-value="history" @change="toggleHistory!" />
+    </div>
+  </el-form-item>
+</template>
+
+<script setup lang="ts">
+import { translate } from '@/utils/i18n'
+import { history, toggleHistory } from '../../stores/history'
+</script>
