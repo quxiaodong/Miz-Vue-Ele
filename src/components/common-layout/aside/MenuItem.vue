d8720c84f9e472f5159f5e6573eacf519396cb71
quxiaodong <quxiaodong1990@gmail.com>
2024-10-04 16:51:13 +0800

    feat: 添加通用布局

---
+++ src/components/common-layout/aside/MenuItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-icon v-if="meta?.icon">
+    <component :is="meta.icon" />
+  </el-icon>
+  {{ meta?.stem ? $t(meta.stem) : meta?.breadcrumb }}
+</template>
+
+<script setup lang="ts">
+import { RouteMeta } from 'vue-router'
+
+type Props = {
+  meta?: RouteMeta
+}
+
+const { meta } = defineProps<Props>()
+</script>
