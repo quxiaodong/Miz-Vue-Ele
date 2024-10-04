ddca33b6eb25c29083cc196bc008424b52f6e490
quxiaodong <quxiaodong1990@gmail.com>
2024-10-04 16:51:13 +0800

    feat: 添加通用布局

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,14 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
+      path: '/',
+      meta: {
+        name: '首页',
+        stem: 'common:menu.home'
+      },
+      component: () => import('@/components/common-layout/Index.vue')
+    },
+    {
       path: '/:pathMatch(.*)*',
       component: () => import('@/views/404/Index.vue')
     }
