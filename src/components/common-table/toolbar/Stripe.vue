96fa47c093f4f683a6077e0c86be6822cef8fed7
quxiaodong <quxiaodong1990@gmail.com>
2024-10-12 11:36:51 +0800

    feat: 添加通用表格

---
+++ src/components/common-table/toolbar/Stripe.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-tooltip placement="top" :content="translate('common:table.stripe')">
+    <el-switch
+      :model-value="stripe"
+      inline-prompt
+      :active-text="translate('common:table.stripe.open')"
+      :inactive-text="translate('common:table.stripe.close')"
+      @change="toggle!"
+    />
+  </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import { translate } from '@/utils/i18n'
+import { useStripeStore } from '../stores/stripe'
+
+const { stripe, toggle } = useStripeStore()!
+</script>
