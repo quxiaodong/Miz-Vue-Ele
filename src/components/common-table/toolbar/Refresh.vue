ff310855884c5bcbc42ff571df737d1037142c5d
quxiaodong <quxiaodong1990@gmail.com>
2024-10-12 11:36:51 +0800

    feat: 添加通用表格

---
+++ src/components/common-table/toolbar/Refresh.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-tooltip placement="top" :content="translate('common:table.refresh')">
+    <el-icon size="18" class="cursor-pointer" @click="click">
+      <RefreshRight />
+    </el-icon>
+  </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import { translate } from '@/utils/i18n'
+import { useApiStore } from '../stores/api'
+
+const { service } = useApiStore()!
+
+const click = () => {
+  if (!service.loading.value) {
+    service.refresh()
+  }
+}
+</script>
