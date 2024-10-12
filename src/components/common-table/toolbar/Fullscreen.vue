336bcc71b7446e822130472a9acfe6ff1a3c4b58
quxiaodong <quxiaodong1990@gmail.com>
2024-10-12 11:36:51 +0800

    feat: 添加通用表格

---
+++ src/components/common-table/toolbar/Fullscreen.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-tooltip placement="top" :content="translate('common:table.fullscreen')">
+    <el-icon size="18" class="cursor-pointer" @click="toggle">
+      <FullScreen />
+    </el-icon>
+  </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import { translate } from '@/utils/i18n'
+import { useFullscreenStore } from '../stores/fullscreen'
+
+const { toggle } = useFullscreenStore()!
+</script>
