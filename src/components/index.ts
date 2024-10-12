457b61a191f80fc0c2887b147c23aaf1c949af3f
quxiaodong <quxiaodong1990@gmail.com>
2024-10-12 11:33:01 +0800

    feat: 添加通用搜索

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,6 +8,7 @@ import CommonDialog from './common-dialog/Index.vue'
 import CommonForm from './common-form/Index.vue'
 import { CommonIcons } from './common-icon'
 import CommonLocale from './common-locale/Index.vue'
+import CommonSearch from './common-search/Index.vue'
 
 const components = (app: App) => {
   app.use(ElementPlus)
@@ -19,6 +20,7 @@ const components = (app: App) => {
   createDialog._context = app._context
   app.component('CommonDialog', CommonDialog)
   app.component('CommonForm', CommonForm)
+  app.component('CommonSearch', CommonSearch)
 }
 
 export default components
