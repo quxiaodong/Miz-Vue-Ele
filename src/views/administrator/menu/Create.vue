be48a433849d9651f9721406087a49533f603624
quxiaodong <quxiaodong1990@gmail.com>
2024-10-18 13:59:47 +0800

    feat: 菜单管理页面

---
+++ src/views/administrator/menu/Create.vue
@@ -0,0 +1,49 @@
+<template>
+  <Form v-model="model" :loading :parent-list @submit="submit" />
+</template>
+
+<script setup lang="ts">
+import {
+  createMenu,
+  CreateMenuInput,
+  searchMenu,
+  SearchMenuOutput
+} from '@/apis/administrator/menu'
+import { ref } from 'vue'
+import Form from './Form.vue'
+
+type Props = {
+  parentId?: string
+}
+
+type Emits = {
+  (e: 'refresh'): void
+}
+
+const { parentId } = defineProps<Props>()
+
+const emits = defineEmits<Emits>()
+
+const initValue = (): CreateMenuInput => ({
+  parentId,
+  name: '',
+  path: '',
+  status: 0
+})
+
+const show = ref(false)
+const model = ref<CreateMenuInput>(initValue())
+const parentList = ref<SearchMenuOutput[]>([])
+
+const { execute, loading } = createMenu()
+
+searchMenu()
+  .execute({})
+  .then(({ data }) => (parentList.value = data))
+
+const submit = () =>
+  execute(model.value).then(() => {
+    show.value = false
+    emits('refresh')
+  })
+</script>
