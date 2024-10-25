ea9192ec065b5af3563c56a624153bd0854a0c75
quxiaodong <quxiaodong1990@gmail.com>
2024-10-25 23:36:40 +0800

    feat: 多语言国际化

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,12 +3,14 @@ import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import { App } from 'vue'
 import { CommonIcons } from './common-icon'
+import CommonLocale from './common-locale/Index.vue'
 
 const components = (app: App) => {
   app.use(ElementPlus)
   for (const [key, component] of Object.entries(CommonIcons)) {
     app.component(key, component)
   }
+  app.component('CommonLocale', CommonLocale)
 }
 
 export default components
