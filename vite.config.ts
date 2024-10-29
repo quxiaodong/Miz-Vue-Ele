4bea353aaa63cb391cf1373c1f4c4a02e29aeeea
quxiaodong <quxiaodong1990@gmail.com>
2024-10-29 19:10:43 +0800

    feat: 按需引入组件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+import {
+  createStyleImportPlugin,
+  ElementPlusResolve
+} from 'vite-plugin-style-import'
+
+const plugins = [vue(), vueJsx()]
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      globsExclude: ['./src/components/**'],
+      resolvers: [ElementPlusResolver()]
+    }),
+    createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+      libs: [
+        {
+          libraryName: 'element-plus',
+          resolveStyle: name => {
+            const names = [
+              'el-message',
+              'el-message-box',
+              'el-notification',
+              'el-descriptions',
+              'el-descriptions-item'
+            ]
+            if (names.includes(name)) {
+              return `element-plus/theme-chalk/${name}.css`
+            }
+            return ''
+          }
+        }
+      ]
+    })
+  )
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,5 +67,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [vue(), vueJsx()]
+  plugins
 })
