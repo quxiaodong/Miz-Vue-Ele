8efd5e8854dd7cc5de6543c2bd9dbbd985dbba00
quxiaodong <quxiaodong1990@gmail.com>
2024-10-30 11:19:49 +0800

    feat: 通用暗黑模式

--- src/typings/global-components.d.ts
+++ src/typings/global-components.d.ts
@@ -1,7 +1,9 @@
+import CommonDark from '@/components/common-dark/Index.vue'
 import CommonLocale from '@/components/common-locale/Index.vue'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     CommonLocale: typeof CommonLocale
+    CommonDark: typeof CommonDark
   }
 }
