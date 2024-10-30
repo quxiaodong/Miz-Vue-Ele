c55bb244c86c2d4d85510ac8c0c897979e01c75c
quxiaodong <quxiaodong1990@gmail.com>
2024-10-30 11:19:49 +0800

    feat: 通用暗黑模式

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,14 @@
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
 import { App } from 'vue'
+import CommonDark from './common-dark/Index.vue'
 
 const components = (app: App) => {
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
   app.use(ElementPlus)
+  app.component('CommonDark', CommonDark)
 }
 
 export default components
