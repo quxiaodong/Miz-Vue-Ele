89dcea8df5bc34687ccffa100a355a53c72d07dd
quxiaodong <quxiaodong1990@gmail.com>
2024-12-16 22:12:58 +0800

    feat: 增删改查部件

---
+++ src/components/common-widget/Link.vue
@@ -0,0 +1,5 @@
+<template>
+  <el-link type="primary" underline="never">
+    <slot />
+  </el-link>
+</template>
---
+++ src/components/common-widget/Update.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-link type="primary" underline="never">
+    <slot>{{ translate('common.update') }}</slot>
+  </el-link>
+</template>
+
+<script setup lang="ts">
+import { translate } from '@/utils/i18n'
+</script>
