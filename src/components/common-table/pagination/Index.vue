quxiaodong/vue3-element-admin

feat: 添加通用表格

src/components/common-table/pagination/Index.vue
@@ -1,0 +1,33 @@
+<template>
+  <el-pagination
+    background
+    :size
+    :layout
+    :pager-count="5"
+    :page-sizes="[10, 20, 30, 40, 50, 100]"
+    :page-size="params.pageSize"
+    :current-page="params.pageNo"
+    :total="service.result.value?.total || 0"
+    class="justify-end sm:justify-center sm:flex-wrap"
+    @size-change="changePageSize"
+    @current-change="changePageNo"
+  />
+</template>
+
+<script setup lang="ts">
+import { useScreenSize } from '@/hooks/useScreenSize'
+import { computed } from 'vue'
+import { useApiStore } from '../stores/useApiStore'
+import { useSizeStore } from '../stores/useSizeStore'
+
+const { isMobile } = useScreenSize()
+
+const { size } = useSizeStore()!
+const { service, params, changePageNo, changePageSize } = useApiStore()!
+
+const layout = computed(() =>
+  isMobile.value
+    ? 'prev, pager, next, total'
+    : 'total, prev, pager, next, sizes'
+)
+</script>

src/components/common-table/toolbar/Fullscreen.vue
@@ -1,0 +1,13 @@
+<template>
+  <el-tooltip :content="$t('common:table.fullscreen')" placement="top">
+    <el-icon size="18" class="cursor-pointer" @click="toggle">
+      <FullScreen />
+    </el-icon>
+  </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import { useFullscreenStore } from '../stores/useFullscreenStore'
+
+const { toggle } = useFullscreenStore()!
+</script>

src/components/common-table/toolbar/Stripe.vue
@@ -1,0 +1,17 @@
+<template>
+  <el-tooltip :content="$t('common:table.stripe')" placement="top">
+    <el-switch
+      :model-value="stripe"
+      inline-prompt
+      :active-text="$t('common:table.stripe.open')"
+      :inactive-text="$t('common:table.stripe.close')"
+      @change="toggle!"
+    />
+  </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import { useStripeStore } from '../stores/useStripeStore'
+
+const { stripe, toggle } = useStripeStore()!
+</script>

quxiaodong/Miz-Vue-Ele

src/components/common-table/pagination/Index.vue
@@ -1,0 +1,33 @@
+<template>
+  <el-pagination
+    background
+    :size
+    :layout
+    :pager-count="5"
+    :page-sizes="[10, 20, 30, 40, 50, 100]"
+    :page-size="params.pageSize"
+    :current-page="params.pageNo"
+    :total="service.result.value?.total || 0"
+    class="mt-4 justify-end sm:justify-center sm:flex-wrap"
+    @size-change="changePageSize"
+    @current-change="changePageNo"
+  />
+</template>
+
+<script setup lang="ts">
+import { useScreenSize } from '@/hooks/useScreenSize'
+import { computed } from 'vue'
+import { useApiStore } from '../stores/api'
+import { useSizeStore } from '../stores/size'
+
+const { isMobile } = useScreenSize()
+
+const { size } = useSizeStore()!
+const { service, params, changePageNo, changePageSize } = useApiStore()!
+
+const layout = computed(() =>
+  isMobile.value
+    ? 'prev, pager, next, total'
+    : 'total, prev, pager, next, sizes'
+)
+</script>

src/components/common-table/toolbar/Fullscreen.vue
@@ -1,0 +1,13 @@
+<template>
+  <el-tooltip :content="$t('common:table.fullscreen')" placement="top">
+    <el-icon size="18" class="cursor-pointer" @click="toggle">
+      <FullScreen />
+    </el-icon>
+  </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import { useFullscreenStore } from '../stores/fullscreen'
+
+const { toggle } = useFullscreenStore()!
+</script>

src/components/common-table/toolbar/Stripe.vue
@@ -1,0 +1,17 @@
+<template>
+  <el-tooltip :content="$t('common:table.stripe')" placement="top">
+    <el-switch
+      :model-value="stripe"
+      inline-prompt
+      :active-text="$t('common:table.stripe.open')"
+      :inactive-text="$t('common:table.stripe.close')"
+      @change="toggle!"
+    />
+  </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import { useStripeStore } from '../stores/stripe'
+
+const { stripe, toggle } = useStripeStore()!
+</script>