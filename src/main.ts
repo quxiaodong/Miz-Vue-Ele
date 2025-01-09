0af1a915b12420812d130f1790798a951ec4dc07
quxiaodong <quxiaodong1990@gmail.com>
2025-01-09 13:10:20 +0800

    feat: 全局插件管理

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import components from './components'
+import plugins from './plugins'
 import router from './router'
 import './styles/index.css'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(components)
+app.use(plugins)
 app.use(router)
 app.mount('#app')
