2c0b722956fb1f0428b61b41d497504e38bc8d05
quxiaodong <quxiaodong1990@gmail.com>
2025-01-09 22:35:13 +0800

    Deploy to Pages

---
+++ assets/math-intrinsics-Cy14X9m_.js
@@ -0,0 +1,1 @@
+var a=Math.abs,r=Math.floor,o=Math.max,s=Math.min,t=Math.pow;export{a,s as b,r as f,o as m,t as p};
---
+++ assets/navigate-Df5rOIDF.js
@@ -0,0 +1,1 @@
+import{I as e}from"./index-Ble-r9fk.js";const c={back:a=>{window.history.length>2?e.back():e.push((a==null?void 0:a.link)||"/")}};export{c as n};
