25f4dbb745224b32133b0b993573c07f0cdf5896
quxiaodong <quxiaodong1990@gmail.com>
2025-02-25 11:24:44 +0800

    feat: 翻译管理页面

---
+++ src/views/language/translation/Index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <common-search
+      v-model:model="searchModel"
+      v-model:params="searchParams"
+      :service="searchService"
+      :init-value
+      :schemas
+    />
+    <common-table
+      v-model:params="searchParams"
+      :data
+      :columns
+      :service="searchService"
+    >
+      <template #toolbar-right>
+        <Create @refresh="searchService.refresh" />
+      </template>
+    </common-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  searchTranslation,
+  SearchTranslationInput
+} from '@/apis/language/translation'
+import dayjs from 'dayjs'
+import { computed, ref } from 'vue'
+import Create from './Create.vue'
+import { useTable } from './useTable'
+
+const initValue = (): SearchTranslationInput => ({
+  pageNo: 1,
+  pageSize: 10
+})
+
+const searchModel = ref(initValue())
+
+const searchService = searchTranslation()
+const searchParams = ref(initValue())
+
+const data = computed(() =>
+  (searchService.result.value?.data ?? []).map(v => {
+    v.createAt = dayjs(v.createAt).format('YYYY-MM-DD HH:mm:ss')
+    return v
+  })
+)
+
+const { columns, schemas } = useTable(searchService, searchParams)
+
+searchService.execute(searchParams.value)
+</script>
