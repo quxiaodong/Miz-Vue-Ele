quxiaodong/vue3-element-admin

feat: 添加通用布局

src/router/index.ts
@@ -4,6 +4,14 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
+      path: '/',
+      meta: {
+        breadcrumb: '首页',
+        i18n: 'common:menu.home'
+      },
+      component: () => import('@/components/common-layout/Index.vue')
+    },
+    {
       path: '/sign-up',
       component: () => import('@/views/sign/Index.vue'),
       children: [

quxiaodong/Miz-Vue-Ele

src/router/index.ts
@@ -4,6 +4,14 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
+      path: '/',
+      meta: {
+        breadcrumb: '首页',
+        stem: 'common:menu.home'
+      },
+      component: () => import('@/components/common-layout/Index.vue')
+    },
+    {
       path: '/sign-up',
       component: () => import('@/views/iam/Index.vue'),
       children: [