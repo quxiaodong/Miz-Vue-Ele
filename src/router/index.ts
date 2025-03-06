quxiaodong/vue3-element-admin

feat: 注册登录页面

src/router/index.ts
@@ -4,6 +4,34 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
+      path: '/sign-up',
+      component: () => import('@/views/sign/Index.vue'),
+      children: [
+        {
+          path: '',
+          meta: {
+            breadcrumb: '注册',
+            i18n: 'common:menu.sign-up'
+          },
+          component: () => import('@/views/sign/sign-up/index.vue')
+        }
+      ]
+    },
+    {
+      path: '/sign-in',
+      component: () => import('@/views/sign/Index.vue'),
+      children: [
+        {
+          path: '',
+          meta: {
+            breadcrumb: '登录',
+            i18n: 'common:menu.sign-in'
+          },
+          component: () => import('@/views/sign/sign-in/Index.vue')
+        }
+      ]
+    },
+    {
       path: '/:pathMatch(.*)*',
       component: () => import('@/views/404/Index.vue')
     }

quxiaodong/Miz-Vue-Ele

src/router/index.ts
@@ -4,6 +4,36 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
+      path: '/sign-up',
+      component: () => import('@/views/iam/Index.vue'),
+      children: [
+        {
+          path: '',
+          meta: {
+            whitelist: true,
+            breadcrumb: '注册',
+            stem: 'common:menu.sign-up'
+          },
+          component: () => import('@/views/iam/sign-up/Index.vue')
+        }
+      ]
+    },
+    {
+      path: '/sign-in',
+      component: () => import('@/views/iam/Index.vue'),
+      children: [
+        {
+          path: '',
+          meta: {
+            whitelist: true,
+            breadcrumb: '登录',
+            stem: 'common:menu.sign-in'
+          },
+          component: () => import('@/views/iam/sign-in/Index.vue')
+        }
+      ]
+    },
+    {
       path: '/:pathMatch(.*)*',
       component: () => import('@/views/404/Index.vue')
     }