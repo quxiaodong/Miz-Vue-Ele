c8599ea6882f817c7220d9b49e86c27b33fd1702
quxiaodong <quxiaodong1990@gmail.com>
2025-03-29 23:36:09 +0800

    feat: 添加第三方库

---
+++ src/views/404/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-empty :image class="h-full" :description="translate('/404.description')">
+    <el-button type="primary" tag="a" href="#/">
+      {{ translate('/404.back') }}
+    </el-button>
+  </el-empty>
+</template>
+
+<script setup lang="ts">
+import image from '@/images/404.svg'
+import { translate } from '@/utils/i18n'
+</script>
