43306ed1b710b9d38a00d67386cbf68288e32c44
quxiaodong <quxiaodong1990@gmail.com>
2025-04-07 09:51:45 +0800

    feat: 外链内嵌页面

---
+++ src/views/iframe/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <iframe :src width="100%" height="100%" />
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const src = route.meta.link as string
+</script>
