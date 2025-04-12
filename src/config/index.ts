96bc76c6698ef333a73b2591899baadad4e80a08
quxiaodong <quxiaodong1990@gmail.com>
2025-04-12 18:51:03 +0800

    feat: 添加应用配置

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+export default {
+  title: import.meta.env.VITE_APP_TITLE,
+  lang: 'zhCn',
+  dark: 'auto',
+  layout: {
+    asideWidth: 218,
+    asideVisible: true,
+    asideCollapse: false,
+    color: '#409EFF',
+    animate: 'fade',
+    tab: true,
+    weak: false,
+    gray: false
+  }
+}
