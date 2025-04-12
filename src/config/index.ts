52a7fc2e6b367d662559154f4a952cec4396e613
quxiaodong <quxiaodong1990@gmail.com>
2025-04-12 18:51:03 +0800

    feat: 添加应用配置

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+export default {
+  superadmin: 'superadmin',
+  dark: 'auto',
+  lang: 'zhCn',
+  layout: {
+    asideWidth: 218,
+    asideVisible: true,
+    asideCollapse: false,
+    color: '#409EFF',
+    animate: 'fade',
+    history: true,
+    weak: false,
+    gray: false
+  }
+}
