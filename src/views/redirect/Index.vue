e3a85d622099a933b02feb09513c2669e2cb3553
quxiaodong <quxiaodong1990@gmail.com>
2025-04-13 11:09:48 +0800

    feat: 页面刷新中转

---
+++ src/views/redirect/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+type Props = {
+  redirect: string
+}
+
+const { redirect } = defineProps<Props>()
+
+const router = useRouter()
+
+router.replace(redirect)
+</script>
